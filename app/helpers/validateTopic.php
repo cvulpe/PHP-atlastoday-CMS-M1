12f294aeb56575be87ef5859f0b527ebc7915398
Cristian Vulpe <cristian.vulpe05@gmail.com>
2021-04-03 21:39:49 +0300

    This fixes #1

--- app/helpers/validateTopic.php
+++ app/helpers/validateTopic.php
@@ -10,7 +10,12 @@ function validateTopic($topic)
 
     $existingTopic = selectOne('topics', ['name' => $topic['name']]);
     if ($existingTopic) {
-        array_push($errors, 'Topic name alredy exists');
+        if (isset($topic['update-topic']) && $existingTopic['id'] != $topic['id']) {
+            array_push($errors, 'Topic name alredy exists');
+        }
+        if (isset($topic['add-topic'])) {
+            array_push($errors, 'Topic name alredy exists');
+        }
     }
 
     return $errors;
